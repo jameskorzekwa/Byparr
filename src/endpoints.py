75e3f017458d25cbb3679ed256d620b449c20b51
James Korzekwa <38769771+jameskorzekwa@users.noreply.github.com>
2025-05-06 18:22:46 -0600

    Update endpoints.py

--- src/endpoints.py
+++ src/endpoints.py
@@ -50,7 +50,7 @@ def read_item(request: LinkRequest, sb: SeleniumDep) -> LinkResponse:
     """Handle POST requests."""
     start_time = int(time.time() * 1000)
     logger.info(f'cookies: {request.cookies}')
-    cookieHeader = [f'{x['name']}={x['value']}' for x in request.cookies].join('; ')
+    cookieHeader = '; '.join([f'{x['name']}={x['value']}' for x in request.cookies])
     logger.info(f'cookieHeader: {cookieHeader}')
     sb.driver.request_interceptor = lambda req: req.headers.update({'Cookie': cookieHeader})
     sb.uc_open_with_reconnect(request.url)
